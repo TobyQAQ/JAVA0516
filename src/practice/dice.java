e3a98f226d50fbcfd9ef6fa061180898e1d85e71
TobyQAQ <a0988065874@gmail.com>
2024-05-16 15:01:35 +0800

    0516_2
    隨機數

--- src/practice/dice.java
+++ src/practice/dice.java
@@ -1,4 +1,14 @@
 package practice;
+import java.util.Random;
+
 
 public class dice {
+    public static  void main(String[] args){
+
+     Random a = new Random();
+
+     a.nextInt(50);
+
+     System.out.println(a.nextInt(50));
+    }
 }
