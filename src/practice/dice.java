1b0cf64e312d951110d1d27729be791b43a09239
TobyQAQ <a0988065874@gmail.com>
2024-05-16 15:51:27 +0800

    0516_3
    骰子計數

--- src/practice/dice.java
+++ src/practice/dice.java
@@ -1,14 +1,29 @@
 package practice;
+
 import java.util.Random;
 
 
 public class dice {
-    public static  void main(String[] args){
+    public static void main(String[] args) {
+        int[] score = new int[50];
+        int[] count = new int[6];
+        for (int i = 0; i < 50; i++) {
+            score[i] = RollDice();
+        }
+        for (int i = 0; i < 50; i++) {
+            System.out.println(score[i]);
+        }
+        for (int i = 0; i < 50; i++) {
+            int temp = score[i];
+            count[temp-1]++;
+        }
 
-     Random a = new Random();
-
-     a.nextInt(50);
-
-     System.out.println(a.nextInt(50));
     }
+    public static int RollDice() {
+        Random a = new Random();
+
+    return (a.nextInt(6) + 1);
+    }
+
+
 }
